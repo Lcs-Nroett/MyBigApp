792f133167055e0748205fc6a6d53419c263966c
Nathan Roett <Nroett@lcs.on.ca>
2026-03-02 14:03:19 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Nathan Roett on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
