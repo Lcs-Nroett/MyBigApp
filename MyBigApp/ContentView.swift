48d1ee3bfb18897d5860c945d550e59c7a730e04
Nathan Roett <Nroett@lcs.on.ca>
2026-03-02 15:28:44 -0500

    Trying an Hstakc instead

--- MyBigApp/ContentView.swift
+++ MyBigApp/ContentView.swift
@@ -16,15 +16,15 @@ struct ContentView: View {
         VStack {
             Rectangle()
                 .aspectRatio(2.0/3.0, contentMode: .fit)
-                .containerRelativeFrame(.horizontal, count: 2, spacing:0)
+                .containerRelativeFrame(.vertical, count: 2, spacing:0)
                 .containerRelativeFrame(.horizontal, alignment: .topLeading)
                 Text("Star Rating")
                 Text("Review")
             Text("A timeless epic with more relevance today than ever.")
         }
         .padding()
+        }
     }
-}
 
 #Preview {
     ContentView()
