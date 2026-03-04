f9df0dc08a060e7a52e2e813072d3bd4164f0f32
Nathan Roett <Nroett@lcs.on.ca>
2026-03-04 09:47:59 -0500

    Added in Vstacks to make it on top of eachother

--- MyBigApp/MyBigApppt2.swift
+++ MyBigApp/MyBigApppt2.swift
@@ -9,39 +9,47 @@ import SwiftUI
 
 struct MyBigApppt2: View {
     var body: some View {
+        
         HStack{
             
-            
-            Rectangle()
-                .frame(width: 100, height: 100)
-            Text("Spring")
-                 Text("In spring, all the flowers take flight,Painting fields with a colourful sight.With each gentle breeze,Come blossoms and bees,And the world wakes to warmth and delight.")
-                .multilineTextAlignment(.leading)
-            
+        
+                Rectangle()
+                    .frame(width: 100, height: 100)
+            VStack{
+                Text("Spring")
+                
+                
+                Text("In spring, all the flowers take flight,Painting fields with a colourful sight.With each gentle breeze,Come blossoms and bees,And the world wakes to warmth and delight.")
+                    .multilineTextAlignment(.leading)
+            }
         }
         HStack{
             
             Rectangle()
                 .frame(width: 100, height: 100)
-            Text("Summer")
-               Text("The sumer sun blazes so bright, Long days filled with laughter and light. With beaches and sand, And ice cream in hand, Every evening feels magic and right")
-                   .multilineTextAlignment(.leading)
-            
+            VStack{
+                Text("Summer")
+                
+                Text("The sumer sun blazes so bright, Long days filled with laughter and light. With beaches and sand, And ice cream in hand, Every evening feels magic and right")
+                    .multilineTextAlignment(.leading)
+            }
         }
         
         HStack{
             Rectangle()
                 .frame(width: 100, height: 100)
-            Text("Fall")
-            Text("The autumn brings leaves turning gold, A string that is lovely and bold. The crisp, cool air sings, Of sweaters and swings, And tales by the fire retold")
+            VStack{
+                Text("Fall")
+                Text("The autumn brings leaves turning gold, A string that is lovely and bold. The crisp, cool air sings, Of sweaters and swings, And tales by the fire retold")
+            }
         }
             HStack{
                 
                 
                 Rectangle()
                     .frame(width: 100, height: 100)
-                   Text("Winter")
-                Text("In winter, the world turns to white, A frosty, enchanting delight.")
+                VStack{   Text("Winter")
+                    Text("In winter, the world turns to white, A frosty, enchanting delight.")}
             }
             
             
